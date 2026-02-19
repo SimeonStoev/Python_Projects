994ed09de5c9bcbad962105c716318d94fe42cf7
SimeonStoev <simeon.stoev.95@gmail.com>
2026-02-19 16:39:53 +0200

    Class Student second version

--- ObjectAndClasses/Student.py
+++ ObjectAndClasses/Student.py
@@ -1,34 +1,29 @@
 class Student:
 
-    def __init__(self, name, age, job, salary):
+    def __init__(self, name, age, school):
         self.__name = name
         self.__age = age
-        self.__job = job
-        self.__salary = salary
+        self.__school = school
 
     def get_name(self):
         return self.__name
     def get_age(self):
         return self.__age
-    def get_job(self):
-        return self.__job
-    def get_salary(self):
-        return self.__salary
+    def get_school(self):
+        return self.__school
 
     def set_name(self, name):
         self.__name = name
     def set_age(self, age):
         self.__age = age
-    def set_job(self, job):
-        self.__job = job
-    def set_salary(self, salary):
-        self.__salary = salary
+    def set_school(self, school):
+        self.__school = school
 
     def get_student_info(self):
-        return f"Name: {self.__name}, age: {self.__age}, job: {self.__job}, salary: {self.__salary}"
+        return f"Name: {self.__name}, age: {self.__age}, school: {self.__school}"
 
-student1 = Student("John", "25", "Mathematics", 20000)
-student2 = Student("Elon", "27", "Engineer", 50000)
+student1 = Student("John", "25", "EG")
+student2 = Student("Elon", "27", "PMG")
 print(student1.get_student_info())
 student1.set_age(20)
 print(student1.get_student_info())
